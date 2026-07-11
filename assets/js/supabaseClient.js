a960f584f8729feb10021dbc8cec758f64289714
annarizky <annarizkynifa@gmail.com>
2026-07-11 21:44:57 +0700

    menambahkan login admin

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -1,6 +1,6 @@
 // Konfigurasi Supabase Client
 // GANTI 'YOUR_SUPABASE_URL' dan 'YOUR_SUPABASE_ANON_KEY' dengan kredensial dari dashboard Supabase Anda (Project Settings -> API).
-const SUPABASE_URL = 'https://hzrrlskqesqhpmryzxsn.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://hzrrlskqesqhpmryzxsn.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6cnJsc2txZXNxaHBtcnl6eHNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3NjgyOTYsImV4cCI6MjA5OTM0NDI5Nn0.1sQfTDGV6yheOH2r-mh2X5AccTsosh0hGGpRzoxj-7Q';
 
 let supabaseClient;
